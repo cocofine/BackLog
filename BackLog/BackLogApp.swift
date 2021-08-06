6fd42bf943d8dffa7138d766d763755f6f06565a
ouyangqi <ouyangqi@haohaozhu.com>
2021-08-06 22:14:05 +0800

    Initial Commit

---
+++ BackLog/BackLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackLogApp.swift
+//  BackLog
+//
+//  Created by ouyangqi on 2021/8/6.
+//
+
+import SwiftUI
+
+@main
+struct BackLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackLog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BackLog
+//
+//  Created by ouyangqi on 2021/8/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
